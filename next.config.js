aeef619dc78f0c2a346aed0577868572ff4c9a49
sgfanega <steve.fanega.ii@gmail.com>
2023-05-04 23:48:38 -0500

    testing getstaticprops

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const { config } = require('process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
-
-config.module.rules.push({
-  test: /\.node$/,
-  use: "node-loader"
-})
